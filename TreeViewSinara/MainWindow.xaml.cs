43d400d9e7d5f138b26672b63876c464e6974b42
kimdimka <kimdimka@yandex.ru>
2021-04-03 09:20:51 +1000

    Каркас сделан

--- TreeViewSinara/MainWindow.xaml.cs
+++ TreeViewSinara/MainWindow.xaml.cs
@@ -22,29 +22,62 @@ namespace TreeViewSinara
     /// </summary>
     public partial class MainWindow : Window
     {
+        public tDataTemplateSelector newtDataTemplateSelector;
+
+        public class tDataTemplateSelector : DataTemplateSelector
+        {
+            public override DataTemplate SelectTemplate(object item, DependencyObject container)
+            {
+                FrameworkElement element = container as FrameworkElement;
+                return
+                    element.FindResource("tSel") as DataTemplate;
+            }
+        }
+        private void ExpandAllNodes(TreeViewItem treeItem)
+        {
+            treeItem.IsExpanded = true;
+            foreach (var childItem in treeItem.Items.OfType<TreeViewItem>())
+            {
+                ExpandAllNodes(childItem);
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            newtDataTemplateSelector = new tDataTemplateSelector();
             ProductBoMRepoSql repo = new ProductBoMRepoSql();
             List<TreeBoMDTO> list = repo.getAllTree().ToList();
             ItemCollection allNodes = treeView.Items;
-
             var groupedList = from bomNode in list
                               orderby bomNode.level, bomNode.RequiredPartNo
                               group bomNode by bomNode.level;
 
             Dictionary<string, List<TreeViewItem>> itemChildCollection = new Dictionary<string, List<TreeViewItem>>();
+            treeView.ItemTemplateSelector = newtDataTemplateSelector;
+
+            TreeViewItem item = new TreeViewItem();
+
             foreach (var partNoGroupItem in groupedList)
             {
-                TreeViewItem item = new TreeViewItem();
-
+                item.HeaderTemplate = FindResource("tSel") as DataTemplate;
+                item.HeaderTemplateSelector = newtDataTemplateSelector;
+                item.Header = new { PartNo = partNoGroupItem.Key, Tag = partNoGroupItem.Key };
                 var itemsInner = item.Items;
                 TreeViewItem itemChild = new TreeViewItem();
+                itemChild.HeaderTemplate = FindResource("tSel") as DataTemplate;
+                itemChild.HeaderTemplateSelector = newtDataTemplateSelector;
                 foreach (var bomItem in partNoGroupItem)
                 {
                     itemChild = new TreeViewItem();
-                    itemChild.Tag = bomItem.level;
-                    itemChild.Header = bomItem.RequiredPartNo;
+                    int bomItemlevel = 0;
+                    int.TryParse(bomItem.level, out bomItemlevel);
+                    itemChild.Foreground = new SolidColorBrush(Color.FromRgb(220, (byte)(150*bomItemlevel), (byte)(160 * bomItemlevel)));
+                    itemChild.HeaderTemplate = FindResource("tSel") as DataTemplate;
+                    itemChild.HeaderTemplateSelector = newtDataTemplateSelector;
+                    itemChild.Tag = bomItem.RequiredPartNo;
+                    itemChild.Header = bomItem;
+
                     if (itemChildCollection.ContainsKey(bomItem.PartNo))
                     {
                         List<TreeViewItem> itemsInnerColl = itemChildCollection[bomItem.PartNo];
@@ -81,13 +114,12 @@ namespace TreeViewSinara
                     }
 
                 }//foreach
-                treeView.Items.Add(item);
 
 
 
             }//forEach
-
-
+            treeView.Items.Add(item);
+            treeView.Items.OfType<TreeViewItem>().ToList().ForEach(ExpandAllNodes);
 
 
 
